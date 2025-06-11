4f85785623b6346f7b0dfa0e910bd2e1447acbbc
Sufyan Mehdi <sufyanmehdi54@gmail.com>
2025-06-11 12:20:27 +0000

    Datatypes and ECMA standards commit

--- Test.js
+++ Test.js
@@ -1,1 +1,25 @@
-console.log("sufyan")
+
+
+//  const accountId = 123;
+//  let accountEmail = "sufyanmehdi991@gamil.com";
+//  let accountPassword = "1234";
+//  accountcity = "hyderabad"
+
+// accountEmail = "hca@gmail.com"
+// accountPassword = "0987"
+// accountcity = "mumbai"
+
+// console.log(accountId)
+
+// console.table([accountId, accountEmail, accountPassword]);
+
+
+// console.log(3 + 3);
+
+
+let name = "Syedsufyanmehdi";
+let age = 18;
+let isLoggedIn = true;
+
+// console.log(name, age, isLoggedIn);
+console.log(typeof name)
