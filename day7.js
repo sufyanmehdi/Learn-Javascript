5693ac24ab26b7fe66722021a2df5d95e57a0507
Sufyan Mehdi <sufyanmehdi54@gmail.com>
2025-06-17 06:42:00 +0000

    Day7.jscommit

---
+++ day7.js
@@ -0,0 +1,22 @@
+const JsUser = {
+    name: "phonenahiiphone",
+    age: 20,
+    location:  "Hyderabad",
+    email: "sufyanmehdi@google.com",
+    isLoggedIn: true
+}
+
+console.log(JsUser[""])
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "mehdi"
+}
+
+
+// const {courseInstructor} = course
+
+
+console.log(courseInstructor);
